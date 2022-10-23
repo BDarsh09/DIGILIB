0e91681636c5361d4da5460cd31e355f916caec0
BDarsh09 <darsh.infotech009@gmail.com>
2022-10-23 18:38:19 +0530

    Initial commit of project setup and deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.static(__dirname + '/dist/digilib'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/digilib/index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
